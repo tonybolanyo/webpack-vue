9fdb9ab90c0c534f02489c188b66fa726df75a26
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-06-27 16:50:55 +0200

    Hello Vue World

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-import style from "./main.css";
-import Vue from "vue";
+import Vue from 'vue'
+import App from './App.vue'
 
-const app = new Vue({
-  el: "#app",
-  data: {
-    message: "Hello Vue!"
-  }
-});
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
